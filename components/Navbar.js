1f7db6241c53015e0008eeb5eabaaabdebbe80a5
Chirag Vasava <149458923+ChiragVasava@users.noreply.github.com>
2025-08-23 20:03:38 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,18 @@
+import React from 'react'
 
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-950 text-white flex justify-between px-4 items-center h-13">
+        <div className="logo font-bold text-lg">GetMeaChai</div>
+        <ul className="flex justify-between gap-4">
+            <li>Home</li>
+            <li>About</li>  
+            <li>Projects</li>
+            <li>Login</li>
+            <li>Sign Up</li>
+        </ul>
+        </nav>
+  )
+}
+
+export default Navbar
