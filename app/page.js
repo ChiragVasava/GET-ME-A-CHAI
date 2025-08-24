b9916e61c1a2eacc1eee75a3ba61ee4f5c35c210
Chirag Vasava <149458923+ChiragVasava@users.noreply.github.com>
2025-08-24 23:50:33 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <>
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
       <div className="flex flex-col justify-center text-white items-center gap-4">
         <div className="font-bold flex gap-2 text-4xl justify-center items-center">Buy Me a Chai
-          <span><img src="/tea.gif" width={80} alt="tea" /></span>
+          <span><img src="tea.gif" width={80} alt="tea" /></span>
         </div>
         <p>
           A crowdfunding platform for creators. Get funded by your fans and followers. Start now!
@@ -19,21 +20,21 @@ export default function Home() {
       <div className="bg-white h-1 opacity-10">
       </div>
 
-      <div className="text-white container mx-auto py-32">
+      <div className="text-white container mx-auto pb-32 pt-14">
         <h1 className="text-2xl font-bold text-center mb-14">Your Fans can buy you a Chai</h1>
         <div className="flex gap-5 justify-around">
           <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/man.gif" alt="man" />
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="man.gif" alt="man" />
             <p className="font-bold">Fans want to help</p>
             <p className="text-center">Your fans are available for you to help you</p>
           </div>
           <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/coin.gif" alt="man" />
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="coin.gif" alt="man" />
             <p className="font-bold">Fans want to help</p>
             <p className="text-center">Your fans are available for you to help you</p>
           </div>
           <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/group.gif" alt="man" />
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="group.gif" alt="man" />
             <p className="font-bold">Fans want to help</p>
             <p className="text-center">Your fans are available for you to help you</p>
           </div>
@@ -43,26 +44,10 @@ export default function Home() {
 
       <div className="bg-white h-1 opacity-10">
       </div>
-
-      <div className="text-white container mx-auto py-32">
-        <h1 className="text-2xl font-bold text-center mb-14">Learn more</h1>
-        <div className="flex gap-5 justify-around">
-          <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/man.gif" alt="man" />
-            <p className="font-bold">Fans want to help</p>
-            <p className="text-center">Your fans are available for you to help you</p>
-          </div>
-          <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/coin.gif" alt="man" />
-            <p className="font-bold">Fans want to help</p>
-            <p className="text-center">Your fans are available for you to help you</p>
-          </div>
-          <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/group.gif" alt="man" />
-            <p className="font-bold">Fans want to help</p>
-            <p className="text-center">Your fans are available for you to help you</p>
-          </div>
-        </div>
+      
+      <div className="text-white container mx-auto pb-32 pt-14">
+        <h1 className="text-2xl font-bold text-center mb-14 text-black">Learn more</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/QtaorVNAwbI?si=x9uTrfWcVYMLSQN5" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
     </>
   );
