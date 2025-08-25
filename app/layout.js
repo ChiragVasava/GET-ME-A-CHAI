172f7aa6127d5c6883e1446a9fc5f5d8556f9dbe
Chirag Vasava <149458923+ChiragVasava@users.noreply.github.com>
2025-08-26 01:37:38 +0530

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Get Me a Chai - Fund Your Projects with chai",
+  title: "Get Me A Chai - Fund Your Projects with chai",
   description: "This website is a crowfunding platform to fund projects with chai.",
 };
 
@@ -24,13 +25,13 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
-
-        <div className="min-h-[85vh]">
+        <SessionWrapper>
+          <Navbar />
+          <div className="min-h-[85vh] fixed inset-0 z-[-2] h-full w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white">
+          </div>
           {children}
-        </div>
-        <Footer />
+          <Footer />
+        </SessionWrapper>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
       <div className="flex flex-col justify-center text-white items-center gap-4">
         <div className="font-bold flex gap-2 text-4xl justify-center items-center">Buy Me a Chai
-          <span><img src="tea.gif" width={80} alt="tea" /></span>
+          <span>
+            <Image src="/tea.gif" width={80} height={80} alt="tea" />
+          </span>
         </div>
         <p>
           A crowdfunding platform for creators. Get funded by your fans and followers. Start now!
@@ -21,20 +24,20 @@ export default function Home() {
       </div>
 
       <div className="text-white container mx-auto pb-32 pt-14">
-        <h1 className="text-2xl font-bold text-center mb-14">Your Fans can buy you a Chai</h1>
+        <h2 className="text-2xl font-bold text-center mb-14">Your Fans can buy you a Chai</h2>
         <div className="flex gap-5 justify-around">
           <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="man.gif" alt="man" />
+            <Image className="bg-slate-400 rounded-full p-2 text-black" width={88} height={88} src="/man.gif" alt="man" />
             <p className="font-bold">Fans want to help</p>
             <p className="text-center">Your fans are available for you to help you</p>
           </div>
           <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="coin.gif" alt="man" />
+            <Image className="bg-slate-400 rounded-full p-2 text-black" width={88} height={88} src="/coin.gif" alt="coin" />
             <p className="font-bold">Fans want to help</p>
             <p className="text-center">Your fans are available for you to help you</p>
           </div>
           <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="group.gif" alt="man" />
+            <Image className="bg-slate-400 rounded-full p-2 text-black" width={88} height={88} src="/group.gif" alt="group" />
             <p className="font-bold">Fans want to help</p>
             <p className="text-center">Your fans are available for you to help you</p>
           </div>
@@ -44,9 +47,9 @@ export default function Home() {
 
       <div className="bg-white h-1 opacity-10">
       </div>
-      
+
       <div className="text-white container mx-auto pb-32 pt-14">
-        <h1 className="text-2xl font-bold text-center mb-14 text-black">Learn more</h1>
+        <h2 className="text-2xl font-bold text-center mb-14 text-white">Learn more</h2>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/QtaorVNAwbI?si=x9uTrfWcVYMLSQN5" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
     </>
